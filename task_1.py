f839a7c4091aa30cbf2199b4a52db43fc4c63653
Andrei Kavalenka <andrew.kovalenko65@gmail.com>
2023-03-13 20:12:58 +0100

    Add documentation to first task

--- task_1.py
+++ task_1.py
@@ -1,11 +1,44 @@
-def is_valid(i, j, matrix, visited):
+def is_valid(i: int, j: int, matrix: list[list[int]],
+             visited: list[list[bool]]) -> bool:
+    """Check if dot valid for next step. Valid conditions: indexes is valid,
+    dot isn't visited and dot is island dot.
+
+    Parameters
+    ----------
+    i: int
+        Row indexer.
+    j: int
+        Column indexer.
+    matrix: list[list[int]]
+        Islands matrix.
+    visited: list[list[bool]]
+        Visited points matrix.
+
+    Returns
+    -------
+    bool
+        If dot is valid for next step or not.
+    """
     # Row number is in range, column number
     # is in range, not yet visited and matrix value is 1
     return (0 <= i < len(visited) and 0 <= j < len(visited[0]) and
             not visited[i][j] and matrix[i][j])
 
 
-def dfs(i, j, matrix, visited):
+def dfs(i: int, j: int, matrix: list[list[int]], visited: list[list[bool]]):
+    """Recurrent depth-first search, marked all neighbours dots as visited.
+
+    Parameters
+    ----------
+    i: int
+        Row indexer.
+    j: int
+        Column indexer.
+    matrix: list[list[int]]
+        Islands matrix.
+    visited: list[list[bool]]
+        Visited points matrix.
+    """
     # Indexes increments from current position to dots that are neighbours.
     row_ind = [-1, 0, 0, 1]
     col_ind = [0, -1, 1, 0]
@@ -19,7 +52,18 @@ def dfs(i, j, matrix, visited):
             dfs(i + row_ind[k], j + col_ind[k], matrix, visited)
 
 
-def islands_counter(matrix):
+def islands_counter(matrix: list[list[int]]):
+    """Main function that iterates through all elements in islands matrix.
+
+    Parameters
+    ----------
+    matrix: list[list[int]]
+        Islands matrix.
+
+    Returns
+    -------
+        Islands count.
+    """
     # Visited points matrix
     visited = [[False for _ in range(len(matrix[0]))] for _ in
                range(len(matrix))]
